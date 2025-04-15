dde50af1e8d137443d9f0829dcb5929a3f37ebbb
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-04-15 14:36:27 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+public class main{
+    public static void main(String args[]){
+        HashSet<String> piyush = new HashSet<String>();
+        piyush.add("kumar");
+        piyush.add("singh");
+        System.out.println("name:"+ piyush);
+    }
+}
